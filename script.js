83538678062e073dc12dc3bbfea66737cf54916f
Ankush kumar <ankushkumarsingh374@gmail.com>
2024-02-22 22:10:42 +0530

    categories,reviw,blogs

--- script.js
+++ script.js
@@ -42,10 +42,9 @@ window.onscroll = () => {
 var swiper = new Swiper(".product-slider", {
   loop: true,
   spaceBetween: 20,
-  autoplay:
-    {
+  autoplay:{
     delay: 7500,
-    disableOnIntraction: false,
+    disableOnInteraction: false,
   },
 
   breakpoints: {
@@ -63,3 +62,28 @@ var swiper = new Swiper(".product-slider", {
   },
 },
   });
+
+
+  var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay:{
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+  
+    breakpoints: {
+    0: {
+      slidesPerView: 1,
+  
+    },
+    768: {
+      slidesPerView: 2,
+  
+    },
+    1020: {
+      slidesPerView: 3,
+  
+    },
+  },
+    });
